f55eb23d2c30709fd56c458676a0d1fdad8a7ca1
safislam1996 <safi.096z@gmail.com>
2022-07-04 10:52:19 +0600

    first commit

---
+++ script.py
@@ -0,0 +1,60 @@
+import functools
+import json
+from deep_translator import GoogleTranslator
+from numpy import dtype
+from threading import Thread
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from fpdf import FPDF
+l=open('languages.json')
+f=open('data.json')
+languages=json.load(l)
+
+file=json.load(f)
+base_url = "https://translate.google.com/?"
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+file_language=list(file[0]['languages'])
+
+list_key=[]
+for i in file_language:
+    i=i.lower()
+    for x,y in languages.items():
+        if(x==i):
+            list_key.append(y)
+# print(list_key)
+pdf = FPDF()  
+pdf.add_page() 
+for tl in list_key:
+    print(tl)
+    url=base_url+"sl=en"+"&tl="+tl+"&op=translate"
+    print(url)
+    driver.get(url)
+    source=driver.find_element_by_class_name('er8xn')
+    string=file[0]['text']
+    string=string.replace('.',' ')
+    print(string)
+    result=source.send_keys(string)
+    driver.implicitly_wait(20)
+    translate=driver.find_element_by_xpath("//span[@class='Q4iAWc']")
+    driver.implicitly_wait(10) 
+    print(translate.text)
+    
+    # pdf.set_font("Arial", size = 15) 
+    # pdf.multi_cell(200, 10, txt = i,  
+    #      align = 'C')
+    # pdf.multi_cell(200, 10, txt =translate.text, 
+    #       align = 'C')
+
+# pdf.output("PP1.pdf") 
+driver.close()  
+
+# sl="en"
+
+# tl="bn"
+
+# 
+# print(url)
+# driver.get(url)
+
